2467e5dc2e2a42f3018bf996b179f431ee814060
Testing_author <testing_beta@mail.com>
2020-03-18 09:57:09 +0200

    Added the "background" SKLabelNode and the "gameScore" SKLabelNode.

--- Whack-a-Meerkat/GameScene.swift
+++ Whack-a-Meerkat/GameScene.swift
@@ -11,14 +11,32 @@ import SpriteKit
 
 class GameScene: SKScene {
 
+    var gameScore: SKLabelNode!
+    
+    var score = 0 {
+    didSet {
+        gameScore.text = "Score: \(score)"
+        }
+    }
+    
+    
     
     override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "whackBackroung")
+        background.position = CGPoint(x: 512, y:384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
         
-        // Get label node from scene and store it for use later
+        gameScore = SKLabelNode(fontNamed: "Chalkduster")
+        gameScore.text = "Score: 0"
+        gameScore.position = CGPoint(x: 8, y: 8)
+        gameScore.horizontalAlignmentMode = .left
+        gameScore.fontSize = 48
+        addChild(gameScore)
     
     }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        <#code#>
     }
     
 }
